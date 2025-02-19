6db23804b6c0c21d13e1d24f99f2f1d0911bf787
SJarod <jarsengkeoPro@gmail.com>
2025-02-19 20:01:28 +0100

    hardcoded

--- Assets/FluidParticle.hlsl
+++ Assets/FluidParticle.hlsl
@@ -1,6 +1,8 @@
 
 void ApplyVelocityField(inout VFXAttributes attributes, VFXSampler2D velocityField)
 {
-    float4 v = velocityField.t.SampleLevel(velocityField.s, attributes.position.xy, 0.0);
-    attributes.velocity = float3(v.x, 0.0, v.y);
+    float2 pos = (-attributes.position.xz + 5.0) / 10.0;
+    float4 v = velocityField.t.SampleLevel(velocityField.s, pos, 0.0);
+    
+    attributes.velocity = float3(-v.r, 0.0, -v.g);
 }
